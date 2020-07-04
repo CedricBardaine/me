7a5fbbf6bcc676c36a61a61847a6d0ecf326b044
Cédric B <cedricbardaine@gmail.com>
2020-07-04 12:26:10 +0200

    add click action on the link instead of the div

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,45 @@
             <b-row style="height: 100%;">
                 <b-col cols="2" id="navMenu">
                     
-                    <div class="menuLink" v-on:click="currentPage = 'home'">
-                        <router-link class="menuLink" v-bind:class="{menuLink_current: currentPage == 'home'}" to="/"> 
+                    <div class="menuLink">
+                        <router-link 
+                        class="menuLink" 
+                        v-bind:class="{menuLink_current: currentPage == 'home'}" 
+                        to="/"> 
                         <!-- In order to make the icon bigger when current page, it must has a :active css class dedicated because the normal fon-size is 2rem and the :active of menu item make it 2rem si we don't see for the moment, but it's cool. -->
-                            <!-- <b-icon class="bigIcon" icon="house-fill"></b-icon>  -->
+                        <!-- <b-icon class="bigIcon" icon="house-fill"></b-icon>  -->
+                        <span v-on:click="currentPage = 'home'">
                             <font-awesome-icon class="bigIcon" icon="home" />
-                        </router-link> <br>
-                    </div>
-                    <div class="menuLink" v-on:click="currentPage = 'about'">
-                        <router-link class="menuLink" v-bind:class="{menuLink_current: currentPage == 'about'}" to="/about">About</router-link> <br>
-                    </div>
-                    
-                    <div @click="switchMode" class="switchBtn">
-                        <b-icon v-if="isNightModeOn"    class="bigIcon" icon="sun"></b-icon>
-                        <b-icon v-else                  class="bigIcon iconDark" icon="moon"></b-icon>
-                    </div>
-                    
-                </b-col>
-                
-                <b-col cols="10">
-                    <b-container class="currentPage">
-                        <router-view/>
-                    </b-container>
-                </b-col>
-            </b-row>
+                        </span> 
+                    </router-link> <br>
+                </div>
+                <div class="menuLink">
+                    <router-link 
+                    class="menuLink" 
+                    v-bind:class="{menuLink_current: currentPage == 'about'}" 
+                    to="/about">
+                    <span v-on:click="currentPage = 'about'">
+                        About
+                    </span>
+                </router-link> <br>
+            </div>
             
-        </b-container>
-    </div>
+            <div class="switchBtn">
+                <b-icon @click="switchMode" v-if="isNightModeOn"    class="bigIcon" icon="sun"></b-icon>
+                <b-icon @click="switchMode" v-else                  class="bigIcon iconDark" icon="moon"></b-icon>
+            </div>
+            
+        </b-col>
+        
+        <b-col cols="10">
+            <b-container class="currentPage">
+                <router-view/>
+            </b-container>
+        </b-col>
+    </b-row>
+    
+</b-container>
+</div>
 </template>
 
 
